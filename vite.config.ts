b52ec3646c5e3cd3a8d92ca528f13d63a1760a4f
hyy110 <hqj678@gmail.com>
2024-03-18 21:15:00 +0800

    feat: add alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import UnoCSS from 'unocss/vite';
+import { join } from 'path';
 
 const Plugins = [UnoCSS(), react()];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: Plugins
+  plugins: Plugins,
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src/')
+    }
+  }
 });
