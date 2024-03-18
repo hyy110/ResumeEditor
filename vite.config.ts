35b8471420f74fd1a8cbba47ebfb3dd11e6632bb
hyy110 <hqj678@gmail.com>
2024-03-18 11:10:53 +0800

    feat: add unocss and scss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
+
+const Plugins = [
+  UnoCSS(),
+  react()
+]
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: Plugins,
 })
