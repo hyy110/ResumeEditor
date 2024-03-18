f447337cfdd11f392bb2710956e2f78adbfb77c4
hyy110 <hqj678@gmail.com>
2024-03-18 20:52:56 +0800

    fix: fix stylelint releases error

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -10,8 +10,6 @@ module.exports = {
     'stylelint-config-standard-scss',
     // 样式属性顺序规则
     'stylelint-config-recess-order',
-    // 接入 Prettier 规则
-    // 'stylelint-config-prettier',
     'stylelint-prettier/recommended'
   ],
   // 配置 rules
